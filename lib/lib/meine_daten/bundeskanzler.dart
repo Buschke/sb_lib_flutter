bc5be38fd839e017498e17e3916a95f57293a855
Sven Buschke <sven@buschke.com>
2023-08-28 16:30:58 +0200

    Diverse Datenlisten.

---
+++ lib/lib/meine_daten/bundeskanzler.dart
@@ -0,0 +1,11 @@
+List<String> bundeskanzler = [
+  'Konrad Adenauer',
+  'Ludwig Erhard',
+  'Kurt Georg Kiesinger',
+  'Willy Brandt',
+  'Helmut Schmidt',
+  'Helmut Kohl',
+  'Gerhard Schröder',
+  'Angela Merkel',
+  'Olaf Scholz',
+];
