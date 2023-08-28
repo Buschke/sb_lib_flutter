d671489085812aba4caf52d2b22897fc1ecb1837
Sven Buschke <sven@buschke.com>
2023-08-28 07:45:38 +0200

    Test-Gruppierung in Hauptseite.

--- test/main_test.dart
+++ test/main_test.dart
@@ -4,18 +4,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sb_fl_app/main.dart';
 
 void main() {
-  testWidgets(
-     'Hauptseite existiert.',
-     (WidgetTester tester) async {
-         const testWidget = PlatformApp(
-             home: Hauptseite(schluessel: Key('HauptseiteSchluessel')),
-         );
-  
-         await tester.pumpWidget(testWidget);
-         await tester.pumpAndSettle();
-  
+  group('Hauptseite', () {
+    testWidgets(
+      'Hauptseite existiert.',
+      (WidgetTester tester) async {
+        const testWidget = PlatformApp(
+          home: Hauptseite(schluessel: Key('HauptseiteSchluessel')),
+        );
+
+        await tester.pumpWidget(testWidget);
+        await tester.pumpAndSettle();
+
         //  final k1 = find.byKey(const Key('k1Schluessel'));
         //  expect(k1, findsOneWidget);
-     },
-  );
+      },
+    );
+  });
 }
