3d980c6f08bf0c0faba03f2cffccfd4b29e7d405
Sven Buschke <sven@buschke.com>
2023-08-28 08:27:42 +0200

    Semikolon eingefügt.

--- test/lib/widgets/platform_widgets/platform_slider_test.dart
+++ test/lib/widgets/platform_widgets/platform_slider_test.dart
@@ -6,18 +6,18 @@ import 'package:sb_fl_app/lib/widgets/platform_widgets/platform_slider.dart';
 void main() {
   group('Slider', () {
     testWidgets(
-       'Slider existiert.',
-       (WidgetTester tester) async {
-           const testWidget = PlatformApp(
-               home: MeinSlider(schluessel: Key('SliderSchluessel')),
-           );
-    
-           await tester.pumpWidget(testWidget);
-           await tester.pumpAndSettle();
-    
-          //  final k1 = find.byKey(const Key('k1Schluessel'));
-          //  expect(k1, findsOneWidget);
-       },
+      'Slider existiert.',
+      (WidgetTester tester) async {
+        const testWidget = PlatformApp(
+          home: MeinSlider(schluessel: Key('SliderSchluessel')),
+        );
+
+        await tester.pumpWidget(testWidget);
+        await tester.pumpAndSettle();
+
+        //  final k1 = find.byKey(const Key('k1Schluessel'));
+        //  expect(k1, findsOneWidget);
+      },
     );
-  })
+  });
 }
