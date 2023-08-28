8c302434f40e78c87ffad135a4630dde8c99c29f
Sven Buschke <sven@buschke.com>
2023-08-28 16:08:02 +0200

    Abstrahiert, List kommt als Parameter

--- lib/lib/widgets/platform_widgets/platform_list_tile.dart
+++ lib/lib/widgets/platform_widgets/platform_list_tile.dart
@@ -3,21 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class PraesidentenListe extends StatelessWidget {
-  PraesidentenListe({Key? schluessel}) : super(key: schluessel);
+  final List<String> meineListe;
 
-  final List<String> praesidenten = [
-    'George Washington',
-    'John Adams',
-    'Thomas Jefferson',
-    'James Madison',
-    'James Monroe',
-  ];
+  const PraesidentenListe({required this.meineListe, Key? schluessel})
+      : super(key: schluessel);
 
   @override
   Widget build(BuildContext context) => ListView.builder(
-        itemCount: praesidenten.length,
-        itemBuilder: (context, index) => PlatformListTile(
-          title: Text(praesidenten[index]),
-        ),
+        itemCount: meineListe.length,
+        itemBuilder: (context, index) =>
+            PlatformListTile(title: Text(meineListe[index])),
       );
 }
