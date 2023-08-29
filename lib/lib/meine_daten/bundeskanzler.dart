fe8b67ea24b8bfa8026060d06ee974205afb1fbc
Sven Buschke <sven@buschke.com>
2023-08-29 08:58:44 +0200

    Erweiterte Objektdefinitionen.

--- lib/lib/meine_daten/bundeskanzler.dart
+++ lib/lib/meine_daten/bundeskanzler.dart
@@ -1,4 +1,4 @@
-List<String> bundeskanzler = [
+List<String> _bundeskanzler = [
   'Konrad Adenauer',
   'Ludwig Erhard',
   'Kurt Georg Kiesinger',
@@ -9,3 +9,42 @@ List<String> bundeskanzler = [
   'Angela Merkel',
   'Olaf Scholz',
 ];
+
+class Amtstraeger {
+
+}
+
+class Bundeskanzler extends Amtstraeger {
+  // String name;
+  // String amtsdauer;
+  List<String> bk = _bundeskanzler;
+
+  // Bundeskanzler({required this.name, required this.amtsdauer});
+
+  get getBundeskanzler => bk;
+}
+
+List<String> _bundespraesident = [
+  'Theodor Heuss',
+  'Heinrich Lübke',
+  'Gustav Heinemann',
+  'Walter Scheel',
+  'Karl Carstens',
+  'Richard von Weizsäcker',
+  'Roman Herzog',
+  'Johannes Rau',
+  'Horst Köhler',
+  'Christian Wulff',
+  'Joachim Gauck',
+  'Frank-Walter Steinmeier',
+];
+
+class Bundespraesident extends Amtstraeger {
+  // String name;
+  // String amtsdauer;
+  List<String> bp = _bundespraesident;
+
+  // Bundespraesident({required this.name, required this.amtsdauer});
+
+  get getBundespraesident => bp;
+}
