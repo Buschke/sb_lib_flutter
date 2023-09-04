324877ad161454f5874c1a689bf9e71d87bbc96d
Sven Buschke <sven@buschke.com>
2023-09-04 16:35:33 +0200

    corrected.

--- lib/lib/models/fp/first_class_functions.dart
+++ lib/lib/models/fp/first_class_functions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class FirstClassFunctions {
   int add2(int x) => x + 2;
   int multiply3(int x) => x * 3;
@@ -21,33 +23,22 @@ class FirstClassFunctions {
   }
 
 // Funktionen
-  int add22(int x) => x + 2;
-  int multiply32(int x) => x * 3;
+  int add2(int x) => x + 2;
+  int multiply3(int x) => x * 3;
 
 // Komposition als Callback
-  compose2(Function f, Function g, Function callback2) {
+  compose(Function f, Function g, Function callback2) {
     return callback2(f(g));
   }
-}
 
-class SimpleFirstClassFunktion {
-  int inkrementiere(int x) => x + 1;
-
-  int transformiere(int x, Function f) => f(x);
-}
-
-class Fibonacci {
-  int fibonacci(int n) {
-    if (n == 0 || n == 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
-
-  int retFibonacci(int n) => fibonacci(n);
-
-  Iterable<int> createFibSeq(int count) sync* {
-    var fn = Fibonacci();
-    for (var i = 0; i < count; i++) {
-      yield fn.retFibonacci(i);
-    }
-  }
+// Nutzung
+// main() {
+//   compose(multiply3, add2, (result) {
+//     if (kDebugMode) {
+//       print('result: $result'); // result: 12
+//     }
+//     log(result.toString());
+//     debugPrint(result.toString());
+//   });
+// }
 }
