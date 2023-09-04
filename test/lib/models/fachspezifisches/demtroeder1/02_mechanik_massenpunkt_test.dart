fb77f223031aded2d624d27e9852de6a929be31d
Sven Buschke <sven@buschke.com>
2023-09-04 04:58:17 +0200

    erster Test.

--- test/lib/models/fachspezifisches/demtroeder1/02_mechanik_massenpunkt_test.dart
+++ test/lib/models/fachspezifisches/demtroeder1/02_mechanik_massenpunkt_test.dart
@@ -1,3 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sb_fl_app/lib/models/fachspezifisches/physik/demtroeder1/02_mechanik_massenpunkt.dart';
+
 void main() {
-  group()
+  group('GeschwindigkeitBeschleunigung', () {
+    GeschwindigkeitBeschleunigung gb = GeschwindigkeitBeschleunigung();
+
+    test('zurueckgelegterWeg', () {
+      double actual = gb.zurueckgelegterWeg(50, 4);
+      double matcher = 200;
+      expect(actual, matcher);
+    });
+  });
 }
